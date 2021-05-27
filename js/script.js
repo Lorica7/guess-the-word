81e8c502a430fbf22fb140fee879422b44f5c21c
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-05-27 10:58:20 -0400

    valited input, handle user guess written

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 /* Global Variables */
 
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedDisplay = document.querySelector(".guessed-letters");
 const guessBtn = document.querySelector(".guess");
 const input = document.querySelector(".letter");
 const wordDisplay = document.querySelector(".word-in-progress");
 const remaining = document.querySelector(".remaining");
 const spanRemain = document.querySelector("span");
-const message = document.querySelector(".message")
+const message = document.querySelector(".message");
 const newGameButton = document.querySelector(".play-again");
-const blanks = []
-const word = "test";
-
+const blanks = [];
+const guesses = [];
+let word = "test";
+let guessesLeft = 8;
 
 
 function displayBlanks(word) {
@@ -19,11 +20,48 @@ function displayBlanks(word) {
     }
     wordDisplay.innerText = blanks.join(" ")
 }
+
+function validateInput(guess) {
+    // accept only letters
+    const accept = /[a-zA-Z]/;
+    if (guess.length === 0) {
+        message.innerText = "Please enter a letter"
+    } else if (guess.length > 1) {
+        message.innerText = "Please enter only one letter" 
+    }else if  (!guess.match(accept)) {
+        message.innerText = "You can only enter a single character from A to Z"
+    } else {
+        console.log("good input")
+        return guess
+    }
+}
+
+function makeGuess(letter) {
+    const altLetter = letter.toUpperCase();
+    if (guesses.includes(altLetter)) {
+        message.InnerText = "You've already guessed that letter! Try again."
+        } else {
+        guesses.push(altLetter);
+        guessesLeft -= 1;
+        spanRemain.innerText = `${guessesLeft} guesses`
+        console.log(guesses)
+    }
+}
+
+// Game sequence
+
 displayBlanks(word)
+
+//EVENT LISTENER FOR GUESS BUTTON
 
 guessBtn.addEventListener("click", (e) => {
     e.preventDefault();
+    message.innerText = "";
     const guess = e.target.form[0].value;
     console.log(guess)
-    input.value = "";
+    input.value = ""
+   let validated =  validateInput(guess)
+    if (validated) {
+        makeGuess(validated)
+    }
 })
